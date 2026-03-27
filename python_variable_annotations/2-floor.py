38efa7aeaa62d155c224744ef4a6a583ced783be
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-27 17:55:40 +1100

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A type annotation of a function claculating floor of float. Example of type annotation.
+
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Adds two floats and returns their sum. It's an example of type annotation.
+
+    """
+    return math.floor(n)
