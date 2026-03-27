01237c1f121db0752f87c49046316b2e58bdbb68
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-27 17:58:25 +1100

    n to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+"""
+A type annotation of a function that returns str of float. Example of type annotation.
+
+"""
+
+
+def add(n: float) -> str:
+    """
+    Returns str of float. It's an example of type annotation.
+
+    """
+    return str(n)
