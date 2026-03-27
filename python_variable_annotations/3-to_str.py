ec6b53068546a26fd5f6fa786424386c70fdb12d
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-27 17:59:10 +1100

    chance func name

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -7,7 +7,7 @@ A type annotation of a function that returns str of float. Example of type annot
 """
 
 
-def add(n: float) -> str:
+def to_str(n: float) -> str:
     """
     Returns str of float. It's an example of type annotation.
 
