93eb31dc8f39d33da7eead8c634018c61093a938
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-27 17:52:46 +1100

    concat str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+"""
+A type annotation of a function adding two floats. Example of type annotation.
+
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Adds two floats and returns their sum. It's an example of type annotation.
+
+    """
+    return str1 + str2
