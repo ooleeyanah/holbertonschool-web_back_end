d77030562c8649dbe2b59f8ae809b52a31b19ed0
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-27 17:49:39 +1100

    blank lines everywhere

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+
+
 """
 A type annotation of a function adding two floats. Example of type annotation.
+
 """
+
+
 def add(a: float, b: float) -> float:
     """
     Adds two floats and returns their sum. It's an example of type annotation.
