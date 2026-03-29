c01e846c6c987db35c3e9c605b7fd92e2ab27f14
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-29 15:42:03 +1100

    whitespace

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -8,7 +8,7 @@ Define function that takes str and int/float & returns tuple
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v:Union[int, float]) -> Tuple[str,float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Return tuple from str & int/float
     """
