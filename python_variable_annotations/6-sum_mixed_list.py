c8a0329f2bef0805f008ecf2296f0267420f357d
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-29 15:31:07 +1100

    union for 3.9 (guys update your python pls)

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,10 +5,10 @@
 Defining a function that calculates sum of list of int and float.
 
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Calculates sum of list of floats and ints.
     """
