6b00fc2a6974bb3e64e5c0caf6e8d37cd3f3f287
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-29 15:21:15 +1100

    doc

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+
+"""
+Defining different variables of varying types to show type annotation.
+
+"""
+
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
