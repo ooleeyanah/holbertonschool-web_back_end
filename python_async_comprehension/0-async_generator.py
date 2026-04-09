11c55018f09d8c699db044cadeeb713dcad254f3
Uliana Deshin <uliana.deshin@gmail.com>
2026-04-09 14:50:33 +1000

    async generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A coroutine that loops 10 times with async wait 1 sec to yield random int.
+"""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Async coroutine that loops 10 times, waits 1 second
+    and yields a random int between 0 and 10.
+
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
