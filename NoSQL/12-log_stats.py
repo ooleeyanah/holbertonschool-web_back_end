4d6b4f3ada1280779c21e9f85a7e0858ea82e95f
Uliana Deshin <uliana.deshin@gmail.com>
2026-04-19 16:06:29 +1000

    log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Provides stats about Nginx logs stored in MongoDB."""
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    collection = client.logs.nginx
+
+    print(f"{collection.count_documents({})} logs")
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print("\tmethod {}: {}".format(
+            method, collection.count_documents({"method": method})
+        ))
+    print("{} status check".format(
+        collection.count_documents({"method": "GET", "path": "/status"})
+    ))
